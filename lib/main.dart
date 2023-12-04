ab1b544a7b1ad7d8478248d9eed85ea794683dc4
BuildTools <unconfigured@null.spigotmc.org>
2023-12-03 19:04:09 -0600

    This isn't done but I'm about to go absolutely insane. All I want for Christmas is to be able to use scenebuilder again

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   // This widget is the root of your application.
   @override
@@ -23,62 +23,89 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title});
 
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
 List<String> messages = <String>[];
 bool isBotThinking = false;
+List<bool> isBotThinkingList = <bool>[];
+// Add the value of isBotThinking to the list
+
+Widget _buildListTile(String message, bool isBotThinking, int position) {
+  return ListTile(
+    title: Text(message),
+    tileColor: isBotThinkingList[position] ? Colors.green : Colors.blue,
+  );
+}
 
 
+void _incrementCounter() {
+  setState(() {
 
-  void _incrementCounter() {
-    setState(() {
+    isBotThinkingList.add(false); // TODO figure out how to get rid of this godforsaken thing
+    if (isBotThinking == false) {
+      messages.add("This is where user input will go");
+      messages.add("bool is " + isBotThinking.toString());
+      isBotThinkingList.add(false); // Add the value of isBotThinking to the list
+      isBotThinking = true;
+    }
+    else {
       messages.add("Hello, I am a chatbot. How can I help you?");
-      if (isBotThinking == false) {
-        messages.add("bool is " + isBotThinking.toString());
-        isBotThinking = true;
-      }
-      else {
-        messages.add("bool is " + isBotThinking.toString());
-        isBotThinking = false;
-      }
-    });
-  }
+      messages.add("bool is " + isBotThinking.toString());
+      isBotThinkingList.add(true); // Add the value of isBotThinking to the list
+      isBotThinking = false;
+    }
+  });
+}
 
-  @override
+
+@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-
       body: Container(
-        child: ListView.builder(
-          itemCount: messages.length,
-          itemBuilder: (BuildContext context, int position) {
-            return ListTile(
-              title: Text(messages[position]),
-              tileColor: Colors.green,
-
-            );
-        },
-
-        )
+        child: Column( // Use a Column widget to hold multiple children
+          children: [
+            Expanded( // Use an Expanded widget to fill the remaining space
+              child: ListView.builder(
+                itemCount: messages.length,
+                itemBuilder: (BuildContext context, int position) {
+                  return _buildListTile(messages[position], isBotThinking, position);
+                },
+              ),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Enter your message',
+                labelText: 'What would you like help with today?',
+                floatingLabelAlignment: FloatingLabelAlignment.center,
+              )
+            ),
+            ElevatedButton( // Place the ElevatedButton widget as the last child of the Column widget
+              onPressed: _incrementCounter,
+              child: Text('Send Message'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))
+              ),
+            ),
+            )],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+
+
+
   }
-
-
 }
